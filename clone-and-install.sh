90311127c577932e7592863750fc0f3461b149ab
Magnus Lundstrom <magnuslundstromdk@gmail.com>
2021-10-31 23:43:39 +0100

    added smart_contracts

--- clone-and-install.sh
+++ clone-and-install.sh
@@ -10,11 +10,11 @@ WEB_NAME="nft-platform-web"
 API_URL="https://github.com/magnuslundstrom/nft-platform-api"
 API_NAME="nft-platform-api"
 
-# SMART_CONTRACT_URL=""
-# SMART_CONTRACT_NAME="nft-platform-smart-contract"
+SMART_CONTRACTS_URL="https://github.com/magnuslundstrom/nft-platform-smart-contracts"
+SMART_CONTRACTS_NAME="nft-platform-smart-contracts"
 
-REPO_URLS=("${WEB_URL}" "${API_URL}")
-REPO_NAMES=("${WEB_NAME}" "${API_NAME}")
+REPO_URLS=("${WEB_URL}" "${API_URL}" "${SMART_CONTRACTS_URL}")
+REPO_NAMES=("${WEB_NAME}" "${API_NAME}" "${SMART_CONTRACTS_NAME}")
 
 IDX=0
 for REPO in ${REPO_URLS[*]}
