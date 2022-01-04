b46eee52f2f1f5f21a79d73bc4851378b81d508b
magnuslundstrom <magnuslundstromdk@gmail.com>
2022-01-04 23:03:30 +0100

    update to include data-service

--- variables.sh
+++ variables.sh
@@ -8,5 +8,9 @@ SMART_CONTRACTS_URL="https://github.com/magnuslundstrom/nft-platform-smart-contr
 SMART_CONTRACTS_NAME="nft-platform-smart-contracts"
 SMART_CONTRACT_PATH="../nft-platform-smart-contracts"
 
-REPO_URLS=("${WEB_URL}" "${SMART_CONTRACTS_URL}")
-REPO_NAMES=("${WEB_NAME}" "${SMART_CONTRACTS_NAME}")
+DATA_SERVICE_URL="https://github.com/magnuslundstrom/nft-platform-data-service"
+DATA_SERVICE_NAME="nft-platform-data-service"
+DATA_SERVICE_PATH="../nft-platform-data-service"
+
+REPO_URLS=("${WEB_URL}" "${SMART_CONTRACTS_URL}" "${DATA_SERVICE_URL}")
+REPO_NAMES=("${WEB_NAME}" "${SMART_CONTRACTS_NAME}" "${DATA_SERVICE_NAME}")
